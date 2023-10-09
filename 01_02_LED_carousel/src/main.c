2e1fd8586626092711ac75d022b8b00506975526
qiaoliang <qiaoliang.email@gmail.com>
2023-10-09 16:16:00 +0800

    重构：将delay() 从main.c 中提出来，放在单独的文件中。

--- 01_02_LED_carousel/src/main.c
+++ 01_02_LED_carousel/src/main.c
@@ -1,24 +1,11 @@
 #include <8052.h>
-
-void delay(unsigned int count)  //100ms延时函数
-{
-  for(int i = 0; i < count; i++){
-  TMOD = 0x10; // 定时器1工作在模式1下
-  TH1 = 0x2E;  // 将计数值的高位设置为0x2E
-  TL1 = 0x80;  // 将计数值的低位设置为0x80
-  TR1 = 1;     // 启动定时器1
-  while (TF1 == 0)
-    ;      // 等待定时器1溢出
-  TR1 = 0; // 停止定时器1
-  TF1 = 0; // 清除定时器1的溢出标志位
-  }
-}
+#include "util.h"
 
 void main(void)
 {
   P2 = 0xFE;
   while(1){
-    delay(10);
+    delay(20);
     P2 = ~P2;
     P2 = P2 << 1;
     P2 = ~P2;

c4f72418805cd937a19dda373f32dc6db574ee54
qiaoliang <qiaoliang.email@gmail.com>
2023-10-09 18:08:34 +0800

    fix bug: delay 函数的定时器初始值计算有误，原来忘记了机器周期与时钟周期的关系，也没有计算补数。

--- 02_01_BTN_HOLD_LED/src/main.c
+++ 02_01_BTN_HOLD_LED/src/main.c
@@ -1,15 +1,11 @@
 #include <8052.h>
-
+#include "util.h"
 void main(void)
 {
-  P2_0 = 0x01;
   while(1){
-    if(P3_1 == 0)
-    {
-      P2_0 = 0x00;
-    }else{
-      P2_0 = 0x01;
-    }
+      delay(1000);
+      P2 = 0xFE;
+      delay(1000);
+      P2 = 0xFF;
   }
-
 }

0abd5a09d6c73b78aeca0481b06f72ed38fe6366
qiaoliang <qiaoliang.email@gmail.com>
2023-10-09 18:28:15 +0800

    02-01 使用第1个独立按键控制第1个LED灯。按下则熄灭，松开则点亮

--- 01_02_LED_carousel/src/main.c
+++ 01_02_LED_carousel/src/main.c
@@ -1,16 +1,11 @@
 #include <8052.h>
 #include "util.h"
-
 void main(void)
 {
-  P2_1 = 0x01;
   while(1){
-    if( P3_1 == 0){
-      delay(20);
-      while(P3_1 == 0);
-      delay(20);
-      P2_1 = ~P2_1;
-    }
+      delay(1000);
+      P2 = 0xFE;
+      delay(1000);
+      P2 = 0xFF;
   }
-
 }
--- 02_01_BTN_HOLD_LED/src/main.c
+++ 02_01_BTN_HOLD_LED/src/main.c
@@ -1,11 +1,15 @@
 #include <8052.h>
 #include "util.h"
+
 void main(void)
 {
+  P2_0 = 0x00;
   while(1){
-      delay(1000);
-      P2 = 0xFE;
-      delay(1000);
-      P2 = 0xFF;
+    if( P3_1 == 1){
+      P2_0 = 0x00;
+    }else{
+      P2_0 = 0x01;
+    }
   }
+
 }

4376fcd3093b2c0d9bbb8a07b5937cd6696d0e4d
qiaoliang <qiaoliang.email@gmail.com>
2023-10-31 19:30:17 +0800

    05-01 UART send

--- 05-01-UART-send/src/main.c
+++ 05-01-UART-send/src/main.c
@@ -1,18 +1,19 @@
 #include <8052.h>
 #include <UART.h>
-#include <timer.h>
+#include <Delay.h>
+
 /**
  * 本程序点亮LED后,以 4800bps 发送数据 0x66. 执行发送命令以后,再熄灭LED,延时1秒,循环执行.
  *
  */
 void main(void)
 {
-  UART_init();
+  UART_Init();
   while(1){
     P2 = 0x00;  //发送前,点亮
-    delay(1000);
-    UART_send(0x66);
+    Delay_ms(1000);
+    UART_SendByte(0x66);
     P2 = 0xFF;  // 发送完成以后,熄灭
-    delay(1000);
+    Delay_ms(1000);
   }
 }
